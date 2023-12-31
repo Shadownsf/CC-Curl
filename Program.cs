58cfe0a9c049f3727bc96758b679d0b021bc17e6
aviv-pierrekore <pierre.kore-ext@groupeseloger.com>
2023-12-31 22:46:45 +0100

    [MOD] provided URL from the command line and print

--- Program.cs
+++ Program.cs
@@ -1,56 +1,77 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CommandDotNet;
 
-public class Program
+namespace CC_Curl
 {
-    static int Main(string[] args)
+    public class Program
     {
-        return new AppRunner<Program>().Run(args);
-    }
-
-    [Subcommand]
-    public Curl Curl { get; set; } = null!;
-}
-
-public class Curl
-{
-    [Command(Description = "Get")]
-    public Task GET(string s)
-    {
-        Console.WriteLine(s);
-
-        var subs = s.Split("/").ToList();
-        subs.RemoveAt(1);
-        
-        var protocol = subs[0].AsSpan()[..subs[0].IndexOf(':')];
-        var host = subs[1].AsSpan();
-
-        Console.WriteLine(protocol.ToString());
-        Console.WriteLine(host.ToString());
-
-        for (int i = 2; i < subs.Count; i++)
+        static int Main(string[] args)
         {
-            Console.WriteLine(subs[i]);
+            return new AppRunner<Program>().Run(args);
         }
 
-        return Task.CompletedTask;
+        [Subcommand]
+        public Get Get { get; set; }
     }
 
-    [Command(Description = "Post")]
-    public void POST(string s)
+    public class Get
     {
-        Console.WriteLine(s);
-    }
+        [DefaultCommand]
+        public Task Execute(string s)
+        {
+            var url = s.Split("://").ToList();
 
-    [Command(Description = "Put")]
-    public void PUT(IConsole console)
-    {
-        console.WriteLine("here's the list of stash");
-    }
+            var protocol = url[0];
 
-    [Command(Description = "Delete")]
-    public void DELETE(IConsole console)
-    {
-        console.WriteLine("here's the list of stash");
+            var path = url[1].Split("/").ToList();
+            var hostAndPort = path[0].Split(':');
+            var host = hostAndPort[0];
+            string port = "";
+            if (hostAndPort.Length > 1)
+            {
+                port = hostAndPort[1];
+            }
+            else
+            {
+                if (protocol == "http")
+                {
+                    port = "80";
+                }
+                else if (protocol == "https")
+                {
+                    port = "443";
+                }
+            }
+
+            //Console.WriteLine(protocol);
+            //Console.WriteLine(port);
+
+            //Console.WriteLine(host.ToString());
+
+            //for (int i = 1; i < path.Count; i++)
+            //{
+            //    Console.WriteLine(path[i]);
+            //}
+
+            string protocolMessage = string.Empty;
+            switch (protocol)
+            {
+                case "http":
+                    protocolMessage = "HTTP/1.1";
+                    break;
+                case "https":
+                    protocolMessage = "TLS/SSL";
+                    break;
+                default:
+                    protocolMessage = "HTTP/1.1";
+                    break;
+            }
+
+            var response = string.Format($"connecting to {host}\nSending request GET /get {protocolMessage}\nHost: {host}\nAccept: */*");
+
+            Console.WriteLine(response);
+
+            return Task.CompletedTask;
+        }
     }
 }
